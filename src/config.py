ff563818fe662356daaadefc63af38a2a3da9b22
Seurin Mathieu <matseurin@cegetel.net>
2018-12-19 13:51:50 +0100

    Moved one hot to model, waaaay better

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import json
 import os
 
 from env.doom_tool import vizdoom_basic_creator
-from ray.tune import register_env, Experiment
+from ray.tune import register_env, Experiment, grid_search
 from ray.tune.schedulers import pbt
 
 from ray.rllib.agents import ppo, dqn
@@ -29,15 +29,7 @@ def load_single_config(config_file):
 
 def merge_env_algo_config(config):
     def _check_config_spec(expe_config):
-        # if embedding size == 0, use one-hot vector for env
-        if expe_config["model"]["custom_options"]["text_objective_config"]["embedding_size"] == 0:
-            expe_config["env_config"]["onehot"] = True
-            # todo : super ugly. Should be in model or processor
-            # But cannot use preprocessor because of multiple intputs
-        else:
-            expe_config["env_config"]["onehot"] = False
-
-
+        # can add check to the config to avoid problem
         return expe_config
 
     # Expe config, it's a mess to makes things work
@@ -61,21 +53,33 @@ def merge_env_algo_config(config):
 
 def create_expe_spec(config, n_cpu, n_gpu, exp_dir):
 
+    def _trial_name_creator(trial):
+        return "{}_{}_123".format(trial.trainable_name, trial.trial_id)
+
     # Create env and register it, so ray and rllib can use it
     register_env(config["env_config"]["env"], lambda env_config: vizdoom_basic_creator(env_config))
 
     expe_config = merge_env_algo_config(config)
 
+    # Shouldn't be useful now, automatic in RLLIB
     #trial_resources = {"cpu": expe_config["num_workers"]+3, "gpu": expe_config["num_gpus"]}
+
+    # expe_config["lr"] = grid_search([1e-3, 1e-4, 5e-4, 1e-5, 5e-5])
+    # expe_config["target_network_update_freq"] = grid_search([20000, 40000])
+
 
     experiment = Experiment(name=config["name_expe"],
                             run=config["algo"],
                             stop=config["stop"],
                             config=expe_config,
                             num_samples=config.get("num_samples",1),
-                            checkpoint_freq= 2,
+                            checkpoint_freq= 10,
                             max_failures=2,
-                            local_dir=exp_dir)
+                            local_dir=exp_dir,
+                            # trial_name_creator=tune.function(_trial_name_creator)
+                            # todo : add when available
+                            )
+
 
     return experiment
 
@@ -107,40 +111,9 @@ def load_config(env_config_file, model_config_file,
 
     return full_config
 
-def set_seed(seed):
-
-    import torch
-    import random
-    import tensorflow
-    import numpy as np
-
-    if seed > -1:
-        print('Using seed {}'.format(seed))
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        random.seed(seed)
-        tensorflow.set_random_seed(seed)
-    else:
-        raise NotImplementedError("Cannot set negative seed")
-
-
-def select_agent(config):
-
-    algo = config["algo"].lower()
-    env =  config["env_config"]["env"]
-    expe_config = merge_env_algo_config(config)
-
-    if algo == "ppo":
-        agent = ppo.PPOAgent(config=expe_config,
-                             env=env)
-
-    elif algo == "apex":
-        agent = dqn.ApexAgent(config=expe_config,
-                             env=env)
-    else:
-        raise NotImplementedError("PPO and Apex are available, that's all")
-
-    return agent
+# ================================================
+# = Population based training and grid search ====
+# ================================================
 
 def prepare_pbt_config(expe_config, pbt_config):
     """
@@ -213,6 +186,53 @@ def prepare_pbt_config(expe_config, pbt_config):
 
     return expe_config, pbt_scheduler
 
+def grid_search_overriding(base_config, json_grid_path):
+
+    grid_search_directory = "config/grid_search"
+    grid_search_ext = load_single_config(os.path.join(grid_search_directory, json_grid_path))
+
+    extended_config = override_config_recurs(config=base_config,
+                                         config_extension=grid_search_ext)
+
+    return extended_config
+
+# =====================
+# OTHER RANDOM FUNCTION
+# =====================
+def set_seed(seed):
+
+    import torch
+    import random
+    import tensorflow
+    import numpy as np
+
+    if seed > -1:
+        print('Using seed {}'.format(seed))
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        random.seed(seed)
+        tensorflow.set_random_seed(seed)
+    else:
+        raise NotImplementedError("Cannot set negative seed")
+
+
+def select_agent(config):
+
+    algo = config["algo"].lower()
+    env =  config["env_config"]["env"]
+    expe_config = merge_env_algo_config(config)
+
+    if algo == "ppo":
+        agent = ppo.PPOAgent(config=expe_config,
+                             env=env)
+
+    elif algo == "apex":
+        agent = dqn.ApexAgent(config=expe_config,
+                             env=env)
+    else:
+        raise NotImplementedError("PPO and Apex are available, that's all")
+
+    return agent
 
 
 if __name__ == "__main__":
--- src/run_expe.py
+++ src/run_expe.py
@@ -6,7 +6,7 @@ import argparse
 
 import ray
 
-from config import load_config, create_expe_spec, set_seed, prepare_pbt_config
+from config import load_config, create_expe_spec, set_seed, prepare_pbt_config, grid_search_overriding
 from neural_toolbox import policy_model
 
 parser = argparse.ArgumentParser('Log Parser arguments!')
@@ -16,6 +16,7 @@ parser.add_argument("-env_ext",      type=str)
 parser.add_argument("-model_config", type=str)
 parser.add_argument("-model_ext",    type=str)
 parser.add_argument("-pbt_config",   type=str, help="json file with population base training parameters")
+parser.add_argument("-grid_config",  type=str, help="json file with grid search parameters to search from")
 parser.add_argument("-exp_dir",      type=str, default="out", help="Directory all results")
 parser.add_argument("-seed",         type=int, default=0, help="Random seed used")
 parser.add_argument("-n_cpu",        type=int, default=24, help="How many cpus do you want ?")
@@ -35,9 +36,13 @@ full_config = load_config(env_config_file=args.env_config,
                           model_ext_file=args.model_ext)
 
 if args.pbt_config:
+    assert args.grid_config == None, "Cannot set Population based training and grid search, choose wisely"
     full_config, pbt_scheduler = prepare_pbt_config(full_config, args.pbt_config)
 else:
     pbt_scheduler = None
+
+if args.grid_config:
+    full_config = grid_search_overriding(full_config, args.grid_config)
 
 experiment = create_expe_spec(full_config,
                               n_cpu=args.n_cpu,
